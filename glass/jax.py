ec7e87dc2f59f64f73ee2c926492d489510be9f6
Eleni <eleni.tsaprazi@fysik.su.se>
2023-02-17 17:26:05 +0100

    added import modules

--- glass/jax.py
+++ glass/jax.py
@@ -7,3 +7,10 @@ __version__ = '2023.1'
 import glass._src.shells as shells
 import glass._src.matter as matter
 import glass._src.fields as fields
+import glass._src.galaxies as galaxies
+import glass._src.fields as fields
+import glass._src.intrinsic_alignments as intrinsic_alignments
+import glass._src.lensing as lensing
+import glass._src.math_utils as math_utils
+import glass._src.observations as observations
+import glass._src.points as points
