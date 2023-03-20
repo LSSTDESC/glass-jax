36ccfc8fa13afc94548b361b1e336ad3576f185a
Natalia Porqueres <natalia_porqueres@hotmail.com>
2023-03-20 15:23:39 +0000

    Add test IA

--- glass/_src/intrinsic_alignments.py
+++ glass/_src/intrinsic_alignments.py
@@ -8,8 +8,8 @@ import jax
 
 import numpy as np
 import pylab as plt
+import pyccl as ccl
 
-Aia = 1.0 #FIXME: is this C_IA?
 rho_crit = lib.cvar.constants.RHO_CRITICAL
 
 def tidal_tensor(hpmap):
@@ -33,27 +33,27 @@ def tidal_tensor(hpmap):
 
     return tidal_tensor_sph
 
-def Epsilon1_NLA(cosmo, z, sxx, syy):
-    gz = jc.background.growth_factor(cosmo, 1. / (1+z))
-    Fact = -1 * Aia * 5e-14 * rho_crit * cosmo.Omega_m /gz
+def Epsilon1_NLA(cosmo, z, sxx, syy, A1):
+    gz = ccl.growth_factor(cosmo, 1./(1+z))
+    Fact = -1 * A1 * 5e-14 * rho_crit * cosmo['Omega_m'] /gz
     e1_NLA = Fact * (sxx - syy)
     return e1_NLA
 
-def Epsilon2_NLA(cosmo, z, sxy):
-    gz = jc.background.growth_factor(cosmo, 1./(1+z))
-    Fact = -Aia * 5e-14 * rho_crit * cosmo.Omega_m / gz
+def Epsilon2_NLA(cosmo, z, sxy, A1):
+    gz = ccl.growth_factor(cosmo, 1./(1+z))
+    Fact = -A1 * 5e-14 * rho_crit * cosmo['Omega_m'] / gz
     e2_NLA = 2 * Fact * sxy
     return e2_NLA
 
-def Epsilon1_TT(cosmo, z, sxx, syy):
-    gz = jc.background.growth_factor(cosmo, 1./(1+z))
-    Fact = -5 * Aia * 5e-14 * rho_crit * cosmo.Omega_m / gz**2
+def Epsilon1_TT(cosmo, z, sxx, syy, A2):
+    gz = ccl.growth_factor(cosmo, 1./(1+z))
+    Fact = -5 * A2 * 5e-14 * rho_crit * cosmo['Omega_m'] / gz**2
     e1_TT = Fact * (syy**2 - sxx**2)
     return e1_TT
 
-def Epsilon2_TT(cosmo, z, sxy, sxx, syy):
-    gz = jc.background.growth_factor(cosmo, 1./(1+z))
-    Fact = -5 * Aia * 5e-14 * rho_crit * cosmo.Omega_m / gz**2
+def Epsilon2_TT(cosmo, z, sxy, sxx, syy, A2):
+    gz = ccl.growth_factor(cosmo, 1./(1+z)) # FIXME: use jax growth factor
+    Fact = -5 * A2 * 5e-14 * rho_crit * cosmo['Omega_m'] / gz**2
     e2_TT = Fact * sxy * (sxx + syy)
     return e2_TT
 
@@ -63,11 +63,11 @@ def NLA(cosmo, z, A1, tidal_field):
     syy = tidal_field[:,1]
     sxy = tidal_field[:,2]
 
-    e1_NLA = Epsilon1_NLA(cosmo, z, sxx, syy)
-    e2_NLA = Epsilon2_NLA(cosmo, z, sxy)
+    e1_NLA = Epsilon1_NLA(cosmo, z, sxx, syy, A1)
+    e2_NLA = Epsilon2_NLA(cosmo, z, sxy, A1)
 
     epsilon_NLA = jnp.empty(e1_NLA.shape, dtype=complex)
-    epsilon_NLA = A1 * e1_NLA + 1j * A1 * e2_NLA
+    epsilon_NLA = e1_NLA + 1j * e2_NLA
 
     return epsilon_NLA
 
@@ -77,28 +77,31 @@ def TATT(cosmo, z, A1, bTA, A2, tidal_field, delta):
     syy = tidal_field[:,1]
     sxy = tidal_field[:,2]
 
-    e1_NLA = Epsilon1_NLA(cosmo, z, sxx, syy)
-    e2_NLA = Epsilon2_NLA(cosmo, z, sxy)
+    e1_NLA = Epsilon1_NLA(cosmo, z, sxx, syy, A1)
+    e2_NLA = Epsilon2_NLA(cosmo, z, sxy, A1)
 
-    e1_TT = Epsilon1_TT(cosmo, z, sxx, syy)
-    e2_TT = Epsilon2_TT(cosmo, z, sxy, sxx, syy)
+    e1_TT = Epsilon1_TT(cosmo, z, sxx, syy, A2)
+    e2_TT = Epsilon2_TT(cosmo, z, sxy, sxx, syy, A2)
 
-    epsilon_NLA  = jnp.array(A1 * e1_NLA + 1j * A1 * e2_NLA)
-    epsilon_TATT = jnp.array(A2 * e1_TT + 1j * A2 * e2_TT)
+    epsilon_NLA  = jnp.array(e1_NLA + 1j * e2_NLA)
+    epsilon_TATT = jnp.array(e1_TT  + 1j * e2_TT)
 
-    epsilon_IA_TATT = epsilon_NLA * (1. + bTA * delta) + epsilon_TATT
+    epsilon_IA_TATT = epsilon_NLA * (1. + bTA * delta) + epsilon_TATT #FIXME: check sign bTA
 
     return epsilon_IA_TATT
 
 def get_IA(z, density_planes, A1=0.18, bTA=0.8, A2=0.1, model='NLA'):
 
+    cosmo_ccl = ccl.Cosmology(Omega_c=0.25, Omega_b=0.05, 
+    h=0.7, sigma8 = 0.801, n_s= 0.963,w0=-1.00,wa=0.0, Omega_k=0.0) #FIXME: change to jax cosmo
+
     tidal_tensor_map = tidal_tensor(density_planes)
     cosmo = jc.Planck15() #FIXME pass cosmo as argument (camb -> jax)
 
     if(model == 'TATT'):
-      return TATT(cosmo, z, A1, bTA, A2, tidal_tensor_map, density_planes)
+      return TATT(cosmo_ccl, z, A1, bTA, A2, tidal_tensor_map, density_planes)
     elif(model == 'NLA'):
-      return NLA(cosmo, z, A1, tidal_tensor_map)
+      return NLA(cosmo_ccl, z, A1, tidal_tensor_map)
     else:
       raise ValueError(f'Unknown IA method')
 
---
+++ test_IA.py
@@ -0,0 +1,190 @@
+import numpy as np
+import healpy as hp
+import matplotlib.pyplot as plt
+
+# use the CAMB cosmology that generated the matter power spectra
+import camb
+from cosmology import Cosmology
+
+# GLASS modules: cosmology and everything in the glass namespace
+import glass.shells
+import glass.fields
+import glass.shapes
+import glass.lensing
+import glass.observations
+import glass.camb
+import glass.intrinsic_alignments
+
+# cosmology for the simulation
+h = 0.7
+Oc = 0.25
+Ob = 0.05
+
+# basic parameters of the simulation
+nside = lmax = 256
+
+# set up CAMB parameters for matter angular power spectrum
+pars = camb.set_params(H0=100*h, omch2=Oc*h**2, ombh2=Ob*h**2,
+                       NonLinear=camb.model.NonLinear_both)
+
+# get the cosmology from CAMB
+cosmo = Cosmology.from_camb(pars)
+
+# %%
+# Set up the matter sector.
+
+# shells of 200 Mpc in comoving distance spacing
+zb = glass.shells.distance_grid(cosmo, 0., 3., dx=200.)
+
+# tophat window function for shells
+zs, ws = glass.shells.tophat_windows(zb)
+
+# compute the angular matter power spectra of the shells with CAMB
+#cls = glass.camb.matter_cls(pars, lmax, zs, ws)
+#np.save(cls)
+cls = np.load('cls.npy')
+# compute Gaussian cls for lognormal fields for 3 correlated shells
+# putting nside here means that the HEALPix pixel window function is applied
+gls = glass.fields.lognormal_gls(cls, nside=nside, lmax=lmax, ncorr=3)
+
+# generator for lognormal matter fields
+matter = glass.fields.generate_lognormal(gls, nside, ncorr=3)
+# np.save('cls.npy', cls)
+# %%
+# Set up the lensing sector.
+
+# this will compute the convergence field iteratively
+convergence = glass.lensing.MultiPlaneConvergence(cosmo)
+
+# %%
+# Set up the galaxies sector.
+
+# galaxy density (using 1/100 of the expected galaxy number density for Stage-IV)
+n_arcmin2 = 0.3
+
+# true redshift distribution following a Smail distribution
+z = np.arange(0., 3., 0.01)
+dndz = glass.observations.smail_nz(z, z_mode=0.9, alpha=2., beta=1.5)
+dndz *= n_arcmin2
+
+# compute bin edges with equal density
+nbins = 5
+zedges = glass.observations.equal_dens_zbins(z, dndz, nbins=nbins)
+
+# photometric redshift error
+sigma_z0 = 0.03
+
+# split distribution by tomographic bin, assuming photometric redshift errors
+tomo_nz = glass.observations.tomo_nz_gausserr(z, dndz, sigma_z0, zedges)
+
+# constant bias parameter for all shells
+bias = 1.2
+
+# ellipticity standard deviation as expected for a Stage-IV survey
+sigma_e = 0.27
+# %%
+# Plotting the overall redshift distribution and the
+# distribution for each of the equal density tomographic bins
+
+plt.figure()
+plt.title('redshift distributions')
+sum_nz = np.zeros_like(tomo_nz[0])
+for nz in tomo_nz:
+    plt.fill_between(z, nz, alpha=0.5)
+    sum_nz = sum_nz + nz
+plt.fill_between(z, dndz, alpha=0.2, label='dn/dz')
+plt.plot(z, sum_nz, ls='--', label='sum of the bins')
+plt.ylabel('dN/dz - gal/arcmin2')
+plt.xlabel('z')
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+
+# %%
+# Make a visibility map typical of a space telescope survey, seeing both
+# hemispheres, and low visibility in the galactic and ecliptic bands.
+vis = glass.observations.vmap_galactic_ecliptic(nside)
+# checking the mask:
+hp.mollview(vis, title='LSST-like Survey Mask', unit='Visibility', coord='CG')
+plt.show()
+
+# %%
+# Simulation
+# ----------
+# Simulate maps of lensing and clustering, without actually sampling
+# from them
+
+# Shear maps
+kap_bar = np.stack([np.zeros(hp.nside2npix(nside)) for i in range(5)],axis=0)
+gam1_bar = np.stack([np.zeros(hp.nside2npix(nside)) for i in range(5)],axis=0)
+gam2_bar = np.stack([np.zeros(hp.nside2npix(nside)) for i in range(5)],axis=0)
+gam1_ia_bar = np.stack([np.zeros(hp.nside2npix(nside)) for i in range(5)],axis=0)
+gam2_ia_bar = np.stack([np.zeros(hp.nside2npix(nside)) for i in range(5)],axis=0)
+
+
+# Galaxy density maps
+gal_map = np.stack([np.zeros(hp.nside2npix(nside)) for i in range(5)],axis=0)
+
+ngals = np.zeros(5)
+
+# simulate the matter fields in the main loop, and build up the catalogue
+for i, delta_i in enumerate(matter):
+
+    # compute the lensing maps for this shell
+    convergence.add_window(delta_i, zs[i], ws[i])  
+    kappa_i = convergence.kappa
+    gamm1_i, gamm2_i = glass.lensing.shear_from_convergence(kappa_i)
+
+    # the photometric galaxy distribution in this shell
+    tomo_z_i, tomo_nz_i = glass.shells.restrict(z, tomo_nz, zs[i], ws[i])
+
+    print(zb[i])
+
+    ia = glass.intrinsic_alignments.get_IA(zb[i], delta_i, A1=0.18, bTA=0.8, A2=0.1, model='NLA')
+
+    hp.mollview(np.log10(2+delta_i))
+    plt.show()
+
+    
+
+    hp.mollview(ia.real/np.max(ia.real))
+    plt.show()
+
+    hp.mollview(ia.imag/np.max(ia.imag))
+    plt.show()
+
+
+    ia = glass.intrinsic_alignments.get_IA(zb[i], delta_i, A1=0.18, bTA=0.8, A2=0.1, model='TATT')
+
+    hp.mollview(ia.real/np.max(ia.real))
+    plt.show()
+
+    hp.mollview(ia.imag/np.max(ia.imag))
+    plt.show()
+
+    for j in range(5):
+        # compute the galaxy density in this shell
+        ngal = np.trapz(tomo_nz_i[j], tomo_z_i)
+
+        # Add contribution from this redshift
+        kap_bar[j] += ngal * kappa_i
+        gam1_bar[j] += ngal * gamm1_i
+        gam2_bar[j] += ngal * gamm2_i
+        ngals[j] += ngal
+            
+        gal_map += ngal * delta_i
+
+
+hp.mollview(ia.real)
+plt.show()
+
+hp.mollview(ia.imag)
+
+hp.mollview(kap_bar[0])
+plt.show()
+
+hp.mollview(gal_map[0])
+plt.show()
+
+
