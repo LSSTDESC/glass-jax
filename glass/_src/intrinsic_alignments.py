40a035d1c5b2b64bf86dd2cb2246f05670ec969f
Natalia Porqueres <natalia_porqueres@hotmail.com>
2023-03-20 12:31:11 +0000

    Fix IA model

--- glass/_src/intrinsic_alignments.py
+++ glass/_src/intrinsic_alignments.py
@@ -6,6 +6,9 @@ import pyccl.ccllib as lib
 import healpy as hp
 import jax
 
+import numpy as np
+import pylab as plt
+
 Aia = 1.0 #FIXME: is this C_IA?
 rho_crit = lib.cvar.constants.RHO_CRITICAL
 
@@ -31,55 +34,54 @@ def tidal_tensor(hpmap):
     return tidal_tensor_sph
 
 def Epsilon1_NLA(cosmo, z, sxx, syy):
-    gz = jc.background.growth_factor(cosmo, 1./(1+z))
-    Fact = -1*Aia*5e-14*rho_crit*cosmo.Omega_m #/gz
+    gz = jc.background.growth_factor(cosmo, 1. / (1+z))
+    Fact = -1 * Aia * 5e-14 * rho_crit * cosmo.Omega_m /gz
     e1_NLA = Fact * (sxx - syy)
     return e1_NLA
 
 def Epsilon2_NLA(cosmo, z, sxy):
     gz = jc.background.growth_factor(cosmo, 1./(1+z))
-    Fact = -1*Aia*5e-14*rho_crit*cosmo.Omega_m #/gz
+    Fact = -Aia * 5e-14 * rho_crit * cosmo.Omega_m / gz
     e2_NLA = 2 * Fact * sxy
     return e2_NLA
 
 def Epsilon1_TT(cosmo, z, sxx, syy):
     gz = jc.background.growth_factor(cosmo, 1./(1+z))
-    Fact = -5*Aia*5e-14*rho_crit*cosmo.Omega_m #/gz**2
+    Fact = -5 * Aia * 5e-14 * rho_crit * cosmo.Omega_m / gz**2
     e1_TT = Fact * (syy**2 - sxx**2)
     return e1_TT
 
 def Epsilon2_TT(cosmo, z, sxy, sxx, syy):
     gz = jc.background.growth_factor(cosmo, 1./(1+z))
-    Fact = -5*Aia*5e-14*rho_crit*cosmo.Omega_m #/gz**2
+    Fact = -5 * Aia * 5e-14 * rho_crit * cosmo.Omega_m / gz**2
     e2_TT = Fact * sxy * (sxx + syy)
     return e2_TT
 
-def NLA(cosmo, z, A1, tidal_tensor_map):
+def NLA(cosmo, z, A1, tidal_field):
     
-    s11 = tidal_tensor_map[:,0]
-    s22 = tidal_tensor_map[:,1]
-    s12 = tidal_tensor_map[:,2]
+    sxx = tidal_field[:,0]
+    syy = tidal_field[:,1]
+    sxy = tidal_field[:,2]
 
-    e1_NLA = Epsilon1_NLA(cosmo, z, s11, s22)
-    e2_NLA = Epsilon2_NLA(cosmo, z, s12)
+    e1_NLA = Epsilon1_NLA(cosmo, z, sxx, syy)
+    e2_NLA = Epsilon2_NLA(cosmo, z, sxy)
 
     epsilon_NLA = jnp.empty(e1_NLA.shape, dtype=complex)
-    epsilon_NLA.real = A1 * e1_NLA
-    epsilon_NLA.imag = A1 * e2_NLA
+    epsilon_NLA = A1 * e1_NLA + 1j * A1 * e2_NLA
 
     return epsilon_NLA
 
-def TATT(cosmo, z, A1, bTA, A2, tidal_tensor_map, delta):
+def TATT(cosmo, z, A1, bTA, A2, tidal_field, delta):
 
-    s11 = tidal_tensor_map[:,0]
-    s22 = tidal_tensor_map[:,1]
-    s12 = tidal_tensor_map[:,2]
+    sxx = tidal_field[:,0]
+    syy = tidal_field[:,1]
+    sxy = tidal_field[:,2]
 
-    e1_NLA = Epsilon1_NLA(cosmo, z, s11, s22)
-    e2_NLA = Epsilon2_NLA(cosmo, z, s12)
+    e1_NLA = Epsilon1_NLA(cosmo, z, sxx, syy)
+    e2_NLA = Epsilon2_NLA(cosmo, z, sxy)
 
-    e1_TT = Epsilon1_TT(cosmo, z, s11, s22)
-    e2_TT = Epsilon2_TT(cosmo, z, s12, s11, s22)
+    e1_TT = Epsilon1_TT(cosmo, z, sxx, syy)
+    e2_TT = Epsilon2_TT(cosmo, z, sxy, sxx, syy)
 
     epsilon_NLA  = jnp.array(A1 * e1_NLA + 1j * A1 * e2_NLA)
     epsilon_TATT = jnp.array(A2 * e1_TT + 1j * A2 * e2_TT)
@@ -100,5 +102,3 @@ def get_IA(z, density_planes, A1=0.18, bTA=0.8, A2=0.1, model='NLA'):
     else:
       raise ValueError(f'Unknown IA method')
 
-
-    
