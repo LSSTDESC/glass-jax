Clean up IA example and fix sign in TATT

glass/_src/intrinsic_alignments.py
@@ -10,6 +10,7 @@ import numpy as np
 import pylab as plt
 import pyccl as ccl
 
+Aia = 1.0 #FIXME: is this C_IA?
 rho_crit = lib.cvar.constants.RHO_CRITICAL
 
 def tidal_tensor(hpmap):
@@ -35,25 +36,25 @@ def tidal_tensor(hpmap):
 
 def Epsilon1_NLA(cosmo, z, sxx, syy, A1):
     gz = ccl.growth_factor(cosmo, 1./(1+z))
-    Fact = -1 * A1 * 5e-14 * rho_crit * cosmo['Omega_m'] /gz
+    Fact = -1. * A1 * 5e-14 * rho_crit * cosmo['Omega_m'] / gz
     e1_NLA = Fact * (sxx - syy)
     return e1_NLA
 
 def Epsilon2_NLA(cosmo, z, sxy, A1):
     gz = ccl.growth_factor(cosmo, 1./(1+z))
-    Fact = -A1 * 5e-14 * rho_crit * cosmo['Omega_m'] / gz
+    Fact = -1. * A1 * 5e-14 * rho_crit * cosmo['Omega_m'] / gz
     e2_NLA = 2 * Fact * sxy
     return e2_NLA
 
 def Epsilon1_TT(cosmo, z, sxx, syy, A2):
     gz = ccl.growth_factor(cosmo, 1./(1+z))
-    Fact = -5 * A2 * 5e-14 * rho_crit * cosmo['Omega_m'] / gz**2
+    Fact = 5 * A2 * 5e-14 * rho_crit * cosmo['Omega_m'] / gz**2
     e1_TT = Fact * (syy**2 - sxx**2)
     return e1_TT
 
 def Epsilon2_TT(cosmo, z, sxy, sxx, syy, A2):
-    gz = ccl.growth_factor(cosmo, 1./(1+z)) # FIXME: use jax growth factor
-    Fact = -5 * A2 * 5e-14 * rho_crit * cosmo['Omega_m'] / gz**2
+    gz = ccl.growth_factor(cosmo, 1./(1+z))
+    Fact = 5 * A2 * 5e-14 * rho_crit * cosmo['Omega_m'] / gz**2
     e2_TT = Fact * sxy * (sxx + syy)
     return e2_TT
 
@@ -86,14 +87,14 @@ def TATT(cosmo, z, A1, bTA, A2, tidal_field, delta):
     epsilon_NLA  = jnp.array(e1_NLA + 1j * e2_NLA)
     epsilon_TATT = jnp.array(e1_TT  + 1j * e2_TT)
 
-    epsilon_IA_TATT = epsilon_NLA * (1. + bTA * delta) + epsilon_TATT #FIXME: check sign bTA
+    epsilon_IA_TATT = epsilon_NLA * (1. + bTA * delta) + epsilon_TATT
 
     return epsilon_IA_TATT
 
 def get_IA(z, density_planes, A1=0.18, bTA=0.8, A2=0.1, model='NLA'):
 
     cosmo_ccl = ccl.Cosmology(Omega_c=0.25, Omega_b=0.05, 
-    h=0.7, sigma8 = 0.801, n_s= 0.963,w0=-1.00,wa=0.0, Omega_k=0.0) #FIXME: change to jax cosmo
+    h=0.7, sigma8 = 0.801, n_s= 0.963,w0=-1.00,wa=0.0, Omega_k=0.0)
 
     tidal_tensor_map = tidal_tensor(density_planes)
     cosmo = jc.Planck15() #FIXME pass cosmo as argument (camb -> jax)

test_IA.py
@@ -100,91 +100,26 @@ plt.legend()
 plt.tight_layout()
 plt.show()
 
-
-# %%
-# Make a visibility map typical of a space telescope survey, seeing both
-# hemispheres, and low visibility in the galactic and ecliptic bands.
-vis = glass.observations.vmap_galactic_ecliptic(nside)
-# checking the mask:
-hp.mollview(vis, title='LSST-like Survey Mask', unit='Visibility', coord='CG')
-plt.show()
-
-# %%
-# Simulation
-# ----------
-# Simulate maps of lensing and clustering, without actually sampling
-# from them
-
-# Shear maps
-kap_bar = np.stack([np.zeros(hp.nside2npix(nside)) for i in range(5)],axis=0)
-gam1_bar = np.stack([np.zeros(hp.nside2npix(nside)) for i in range(5)],axis=0)
-gam2_bar = np.stack([np.zeros(hp.nside2npix(nside)) for i in range(5)],axis=0)
-gam1_ia_bar = np.stack([np.zeros(hp.nside2npix(nside)) for i in range(5)],axis=0)
-gam2_ia_bar = np.stack([np.zeros(hp.nside2npix(nside)) for i in range(5)],axis=0)
-
-
-# Galaxy density maps
-gal_map = np.stack([np.zeros(hp.nside2npix(nside)) for i in range(5)],axis=0)
-
-ngals = np.zeros(5)
-
-# simulate the matter fields in the main loop, and build up the catalogue
 for i, delta_i in enumerate(matter):
-
-    # compute the lensing maps for this shell
-    convergence.add_window(delta_i, zs[i], ws[i])  
-    kappa_i = convergence.kappa
-    gamm1_i, gamm2_i = glass.lensing.shear_from_convergence(kappa_i)
-
-    # the photometric galaxy distribution in this shell
-    tomo_z_i, tomo_nz_i = glass.shells.restrict(z, tomo_nz, zs[i], ws[i])
-
-    print(zb[i])
 
     ia = glass.intrinsic_alignments.get_IA(zb[i], delta_i, A1=0.18, bTA=0.8, A2=0.1, model='NLA')
 
     hp.mollview(np.log10(2+delta_i))
     plt.show()
 
-    
 
-    hp.mollview(ia.real/np.max(ia.real))
+    hp.mollview(np.log10(1+ia.real))
     plt.show()
 
-    hp.mollview(ia.imag/np.max(ia.imag))
+    hp.mollview(np.log10(1+ia.imag))
     plt.show()
 
 
     ia = glass.intrinsic_alignments.get_IA(zb[i], delta_i, A1=0.18, bTA=0.8, A2=0.1, model='TATT')
 
-    hp.mollview(ia.real/np.max(ia.real))
+    hp.mollview(np.log10(1+ia.real))
     plt.show()
 
-    hp.mollview(ia.imag/np.max(ia.imag))
+    hp.mollview(np.log10(1+ia.imag))
     plt.show()
-
-    for j in range(5):
-        # compute the galaxy density in this shell
-        ngal = np.trapz(tomo_nz_i[j], tomo_z_i)
-
-        # Add contribution from this redshift
-        kap_bar[j] += ngal * kappa_i
-        gam1_bar[j] += ngal * gamm1_i
-        gam2_bar[j] += ngal * gamm2_i
-        ngals[j] += ngal
-            
-        gal_map += ngal * delta_i
-
-
-hp.mollview(ia.real)
-plt.show()
-
-hp.mollview(ia.imag)
-
-hp.mollview(kap_bar[0])
-plt.show()
-
-hp.mollview(gal_map[0])
-plt.show()
-
 