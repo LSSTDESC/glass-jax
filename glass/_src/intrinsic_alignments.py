a55951db0b9fd5905fb593248a1f4cd0c17d06e1
Natalia Porqueres <natalia_porqueres@hotmail.com>
2023-03-21 16:29:18 +0000

    Remove ccl dependency

--- glass/_src/intrinsic_alignments.py
+++ glass/_src/intrinsic_alignments.py
@@ -6,11 +6,6 @@ import pyccl.ccllib as lib
 import healpy as hp
 import jax
 
-import numpy as np
-import pylab as plt
-import pyccl as ccl
-
-Aia = 1.0 #FIXME: is this C_IA?
 rho_crit = lib.cvar.constants.RHO_CRITICAL
 
 def tidal_tensor(hpmap):
@@ -19,7 +14,7 @@ def tidal_tensor(hpmap):
     nside = hp.get_nside(hpmap)
 
     alm_E = hp.map2alm(hpmap, lmax=5000, mmax=None, iter=3, pol=False, use_weights=False, datapath=None)
-    alm_B = jnp.zeros_like(alm_E)
+    alm_B = jnp.zeros_like(alm_E) # FIXME: jaxify
 
     # smoothing if necessary here
 
@@ -35,26 +30,26 @@ def tidal_tensor(hpmap):
     return tidal_tensor_sph
 
 def Epsilon1_NLA(cosmo, z, sxx, syy, A1):
-    gz = ccl.growth_factor(cosmo, 1./(1+z))
-    Fact = -1. * A1 * 5e-14 * rho_crit * cosmo['Omega_m'] / gz
+    gz = jc.background.growth_factor(cosmo, 1./(1+z))
+    Fact = -1. * A1 * 5e-14 * rho_crit * cosmo.Omega_c / gz
     e1_NLA = Fact * (sxx - syy)
     return e1_NLA
 
 def Epsilon2_NLA(cosmo, z, sxy, A1):
-    gz = ccl.growth_factor(cosmo, 1./(1+z))
-    Fact = -1. * A1 * 5e-14 * rho_crit * cosmo['Omega_m'] / gz
+    gz = jc.background.growth_factor(cosmo, 1./(1+z))
+    Fact = -1. * A1 * 5e-14 * rho_crit * cosmo.Omega_c / gz
     e2_NLA = 2 * Fact * sxy
     return e2_NLA
 
 def Epsilon1_TT(cosmo, z, sxx, syy, A2):
-    gz = ccl.growth_factor(cosmo, 1./(1+z))
-    Fact = 5 * A2 * 5e-14 * rho_crit * cosmo['Omega_m'] / gz**2
+    gz = jc.background.growth_factor(cosmo, 1./(1+z))
+    Fact = 5 * A2 * 5e-14 * rho_crit * cosmo.Omega_c / gz**2
     e1_TT = Fact * (syy**2 - sxx**2)
     return e1_TT
 
 def Epsilon2_TT(cosmo, z, sxy, sxx, syy, A2):
-    gz = ccl.growth_factor(cosmo, 1./(1+z))
-    Fact = 5 * A2 * 5e-14 * rho_crit * cosmo['Omega_m'] / gz**2
+    gz = jc.background.growth_factor(cosmo, 1./(1+z))
+    Fact = 5 * A2 * 5e-14 * rho_crit * cosmo.Omega_c / gz**2
     e2_TT = Fact * sxy * (sxx + syy)
     return e2_TT
 
@@ -93,16 +88,14 @@ def TATT(cosmo, z, A1, bTA, A2, tidal_field, delta):
 
 def get_IA(z, density_planes, A1=0.18, bTA=0.8, A2=0.1, model='NLA'):
 
-    cosmo_ccl = ccl.Cosmology(Omega_c=0.25, Omega_b=0.05, 
-    h=0.7, sigma8 = 0.801, n_s= 0.963,w0=-1.00,wa=0.0, Omega_k=0.0)
-
     tidal_tensor_map = tidal_tensor(density_planes)
     cosmo = jc.Planck15() #FIXME pass cosmo as argument (camb -> jax)
+    redshift = jnp.atleast_1d(z)
 
     if(model == 'TATT'):
-      return TATT(cosmo_ccl, z, A1, bTA, A2, tidal_tensor_map, density_planes)
+      return TATT(cosmo, redshift, A1, bTA, A2, tidal_tensor_map, density_planes)
     elif(model == 'NLA'):
-      return NLA(cosmo_ccl, z, A1, tidal_tensor_map)
+      return NLA(cosmo, redshift, A1, tidal_tensor_map)
     else:
       raise ValueError(f'Unknown IA method')
 
