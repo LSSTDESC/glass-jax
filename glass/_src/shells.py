e51e69595ddb01233853681202f8f93771a13127
Francois Lanusse <fr.eiffel@gmail.com>
2023-03-22 08:22:35 +0100

    added fixes to allow jitting

--- glass/_src/shells.py
+++ glass/_src/shells.py
@@ -1,7 +1,9 @@
 import jax.numpy as jnp
 import jax_cosmo as jc
-
+import numpy as np
 import warnings
+
+from .math_utils import ndinterp
 
 # type checking
 from typing import (Union, Sequence, List, Tuple, Optional, Callable,
@@ -9,6 +11,7 @@ from typing import (Union, Sequence, List, Tuple, Optional, Callable,
 # types
 ArrayLike1D = Union[Sequence[float], jnp.ndarray]
 WeightFunc = Callable[[ArrayLike1D], jnp.ndarray]
+
 
 
 def tophat_windows(zbins: ArrayLike1D, dz: float = 1e-3,
@@ -41,8 +44,8 @@ def tophat_windows(zbins: ArrayLike1D, dz: float = 1e-3,
     '''
     if len(zbins) < 2:
         raise ValueError('zbins must have at least two entries')
-    if zbins[0] != 0:
-        warnings.warn('first tophat window does not start at redshift zero')
+    # if zbins[0] != 0:
+    #     warnings.warn('first tophat window does not start at redshift zero')
 
     wf: WeightFunc
     if wfunc is not None:
@@ -52,11 +55,43 @@ def tophat_windows(zbins: ArrayLike1D, dz: float = 1e-3,
 
     zs, ws = [], []
     for zmin, zmax in zip(zbins, zbins[1:]):
-        n = max(round((zmax - zmin)/dz), 2)
-        z = jnp.linspace(zmin, zmax, n)
+        # THIS WILL NOT JIT
+        # n = max(round((zmax - zmin)/dz), 2)
+        # z = jnp.linspace(zmin, zmax, n)
+        z = np.linspace(0,4,int(4./dz))
         zs.append(z)
         ws.append(wf(z))
     return zs, ws
+
+def restrict(z: ArrayLike1D, f: ArrayLike1D, w: 'RadialWindow'
+             ) -> Tuple[np.ndarray, np.ndarray]:
+    '''Restrict a function to a redshift window.
+    Multiply the function :math:`f(z)` by a window function :math:`w(z)`
+    to produce :math:`w(z) f(z)` over the support of :math:`w`.
+    The function :math:`f(z)` is given by redshifts ``z`` of shape
+    *(N,)* and function values ``f`` of shape *(..., N)*, with any
+    number of leading axes allowed.
+    The window function :math:`w(z)` is given by ``w``, which must be a
+    :class:`RadialWindow` instance or compatible with it.
+    The restriction has redshifts that are the union of the redshifts of
+    the function and window over the support of the window.
+    Intermediate function values are found by linear interpolation
+    Parameters
+    ----------
+    z, f : array_like
+        The function to be restricted.
+    w : :class:`RadialWindow`
+        The window function for the restriction.
+    Returns
+    -------
+    zr, fr : array
+        The restricted function.
+    '''
+    #TODO: use functions instead of tabulated values
+    # z_ = jnp.compress(jnp.greater(z, w.za[0]) & jnp.less(z, w.za[-1]), z)
+    zr = z #jnp.union1d(w.za, z)
+    fr = f * jnp.interp(z, w.za, w.wa)
+    return zr, fr
 
 
 def distance_grid(cosmo, zmin, zmax, *, dx=None, num=None):
