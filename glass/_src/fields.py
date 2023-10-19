9d70fac9232ac7c8d3479d1fb38462935d8ffb4b
Junzhe Zhou <junzhez@login36.chn>
2023-10-19 12:24:40 -0700

    lognormal now depends on gaussian

--- glass/_src/fields.py
+++ glass/_src/fields.py
@@ -4,6 +4,7 @@ import numpy as np
 import healpy as hp
 import s2fft
 from jax import jit, vmap
+from mcfit import C2w, w2C
 
 # typing
 from typing import Any, Union, Tuple, Generator, Optional, Sequence, Callable, Iterable
@@ -36,46 +37,49 @@ def generate_gaussian(gls, nside, L, seed):
         yield s2fft.inverse_jax(alm, L, reality=True, sampling=sampling, nside=nside)
 
 
-def generate_lognormal(gls, shifts, C2w_f, w2C_f, nside, L, seed):
+def generate_lognormal(gls, nside, shift, L, seed):
     # gls have length number of bins, it is just the auto spectra of the matter shells
     # we will just do individual Hankel transforms on each bin since they are not correlated
-
-    sampling = "healpix"
-    mean_pixel_area = 4 * np.pi / hp.nside2npix(nside)
-    scaling_factor = np.sqrt(1 / mean_pixel_area)
-    keys = jax.random.split(seed, len(gls))
-
     # TODO: L max for Hankel transform should be much larger than the target ell range
     # we will just do this approximately here
     # here we just assume C2w and w2C carries information of the ell ranegs of gls
-    ell = jnp.arange(L)
-    ell_log = C2w_f.x
 
-    for i, (cl, shift) in enumerate(zip(gls, shifts)):
-        # TODO: we could perform the Hankel transform in batch if this loop is slow
-        cl = cl[0]  # cl here has shape (1, nells)
+    nshell = len(gls)
+    nell_gls = gls[0].shape[-1]
+    lmax_gls = nell_gls - 1
+    print("nshell", nshell)
+    print("nell", nell_gls)
+
+    # gls along integer ells
+    ell = jnp.arange(nell_gls)
+    # we need log spaced gls'
+    ell_log = jnp.logspace(0, jnp.log10(lmax_gls), 400)
+    gls_log = jnp.array(
+        [(jnp.interp(ell_log, ell, gls[i][0])).reshape([1, -1]) for i in range(nshell)]
+    )
+
+    # Hankel transforms
+    # broadcasting to the last axis
+    C2w_f = C2w(ell_log, backend="jax")  # C -> w function
+    theta = C2w_f.y
+    w2C_f = w2C(theta, backend="jax")  # w -> C function
+
+    # ell_query defined by lmax = L (not inclusive)
+    ell_query = jnp.arange(L)
+
+    # TODO: the Hankel transform can already be done in batch, will rewrite this part later
+    gls_log_gauss = jnp.zeros((nshell, 1, L))
+    for i, (C_ln, a) in enumerate(zip(gls_log, shift)):
+        C_ln = C_ln[0]  # cl here has shape (1, nells)
         C_gauss = _calc_C_gauss(
-            a=shift, C_ln=cl, C2w_f=C2w_f, w2C_f=w2C_f, ell_query=ell
+            a=a, C_ln=C_ln, C2w_f=C2w_f, w2C_f=w2C_f, ell_query=ell_query
         )
-        cl = C_gauss.reshape([1, -1])
-        # Sample gaussian map
-        z = (
-            jax.random.normal(key=keys[i], shape=(hp.nside2npix(nside),))
-            * scaling_factor
-        )
+        C_ln = C_gauss.reshape([1, -1])
 
-        # Convert to alms
-        alm = s2fft.forward_jax(z, L, reality=True, sampling=sampling, nside=nside)
+        gls_log_gauss = gls_log_gauss.at[i].set(C_ln)
 
-        # Multiply alms by power spectrum
-        alm = alm * jnp.sqrt(cl.reshape([L, 1]))
-
-        # Inverse SHT
-        map_g = s2fft.inverse_jax(alm, L, reality=True, sampling=sampling, nside=nside)
-
-        # convert to lognormal
-        mag_ln = (jnp.exp(map_g - jnp.var(map_g) / 2) - 1) * shift
-
+    for i, map_g in enumerate(generate_gaussian(gls_log_gauss, nside, L, seed)):
+        mag_ln = (jnp.exp(map_g - jnp.var(map_g) / 2) - 1) * shift[i]
         yield mag_ln
 
 
