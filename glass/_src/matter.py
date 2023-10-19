45c76dacc2de0d7f994bd8761920b55c08308098
Francois Lanusse <fr.eiffel@gmail.com>
2023-10-19 13:53:47 -0400

    Upgrading matter cls to use the new window

--- glass/_src/matter.py
+++ glass/_src/matter.py
@@ -2,12 +2,12 @@ import jax.numpy as jnp
 import jax_cosmo as jc
 
 
-def matter_cls(cosmo, lmax, zs, ws):
+def matter_cls(cosmo, lmax, win):
     """ Uses jax-cosmo to compute matter angular cls for the 
     given redshift bins.
     """
     #TODO: Replace this by proper top hat window function
-    nzs = [jc.redshift.kde_nz(z, w, bw=(z[1]-z[0]), zmax=4.0) for z, w in zip(zs, ws)]
+    nzs = [jc.redshift.kde_nz(w.za, w.wa, bw=(w.za[1]-w.za[0]), zmax=w.za.max()) for w in win]
     ell = jnp.arange(lmax)
     #CAVEAT: we only compute the autopowerspectra because we don't have the non-limber integral necessary for the cross anyway.
     return [jc.angular_cl.angular_cl(cosmo, ell, [jc.probes.NumberCounts([nz], jc.bias.constant_linear_bias(1.))]) for nz in nzs]
